4b5a5df1d85495eb09216b9b22f6e8ade45da6b3
evegeniy <72518319+evegeniy@users.noreply.github.com>
2021-04-28 11:58:00 +0200

    Creating a database in mysql and connecting to frontend

---
+++ spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Juan', 'Perez', 'juan@domain.com', '2021-06-21');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Maria', 'Suarez', 'maria@domain.com', '2021-02-11');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Lisa', 'Souto', 'lisa@domain.com', '2021-07-13');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Marco', 'Polo', 'marco@domain.com', '2021-07-13');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Carlos', 'Gil', 'carlos@domain.com', '2021-07-13');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Teresa', 'Castro', 'teresa@domain.com', '2021-07-13');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Eva', 'Mendez', 'eva@domain.com', '2021-07-13');
